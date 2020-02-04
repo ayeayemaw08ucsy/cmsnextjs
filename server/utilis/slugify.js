cfd8933105bbf7de3fe74a6d732b753c1df596d6
Aye Aye Maw <ayeayemaw08ucsy@gmail.com>
2020-02-04 06:52:47 +0630

    pages/add-article.js

---
+++ server/utilis/slugify.js
@@ -0,0 +1,25 @@
+const slugify = text =>
+  text
+    .toString()
+    .toLowerCase()
+    .trim()
+    // Replace spaces with -
+    .replace(/\s+/g, '-')
+    // Replace & with 'and'
+    .replace(/&/g, '-and-')
+    // Remove all non-word chars
+    .replace(/(?!\w)[\x00-\xC0]/g, '-') // eslint-disable-line
+    // Replace multiple - with single -
+    .trim('-')
+    .replace(/\-\-+/g, '-') // eslint-disable-line
+    // Remove - from start & end
+    .replace(/-$/, '')
+    .replace(/^-/, '');
+
+
+    async function generateSlug(Moel, name, filter = {})  {
+        const orignalSlug = slugify(name);
+        return orignalSlug;
+    }
+
+    module.exports = generateSlug;
