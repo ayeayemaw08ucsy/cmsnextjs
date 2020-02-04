170174c7ed57814868667d362889573d10f384c0
Aye Aye Maw <ayeayemaw08ucsy@gmail.com>
2020-02-04 09:20:44 +0630

    refactor

--- pages/index.js
+++ pages/index.js
@@ -8,40 +8,17 @@ import { useState } from 'react';
 const Index = props => {
 
   return (
-    <div style={{ padding: '10px 45px' }}>
-    <Header/>
-     { <Articles articles={props.articles} total={props.total} page={props.page}/>}
-     {/* { <ul>
-                {props.articles.map(article =>(
-                  <li key={article._id}>
-                    <a>{article.title}</a>
-                  </li>   
-                ))}
-            </ul>}  */}
-    </div>
+    
+     <Articles articles={props.articles} total={props.total} page={props.page}/>
+    
   )
 }
 
-// const Index = () => {
-  
-//       return (
-//         <div style={{ padding: '10px 45px' }}>
-//         <Head/>
-//          <Articles/>
-//         </div>
-//       )
-//     }
-
-//     Index.getInitialProps = async function() {
-//       return {}
-//     }
-
 Index.getInitialProps = async function(){
+   
     const total = await getArticleCount();
-    console.log("Initial Count" , total)
     const page = total > 5 ? Math.floor(total/5) : 1;
     const res = await getArticleList({page,total});
     return Object.assign({},{total:total},{page:page},res);
-    //return res;
 }
 export default Index;
